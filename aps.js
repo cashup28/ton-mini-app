19eccf4a257f2e60c03e5b3f15bc3712fa9be8e4
cashup28 <acyedek1@gmail.com>
2025-03-20 19:51:17 +0300

    Update aps.js

--- aps.js
+++ aps.js
@@ -7,27 +7,28 @@ const connector = new TonConnectSDK.TonConnect({
 
 const connectBtn = document.getElementById('connect-btn');
 const status = document.getElementById('status');
+const qrContainer = document.getElementById('qr-code');
 
 connectBtn.onclick = async () => {
-    const walletsList = await connector.getWallets();
-    const wallet = walletsList.find(w => w.appName === "tonkeeper") || walletsList[0];
+    const link = connector.connect({ bridgeUrl: "https://bridge.tonapi.io/bridge" });
 
-    if (wallet) {
-        connector.connect({
-            bridgeUrl: wallet.bridgeUrl,
-            universalLink: wallet.universalLink
-        });
+    qrContainer.innerHTML = '';
+    new QRCode(qrContainer, {
+        text: link,
+        width: 220,
+        height: 220,
+    });
 
-        connector.onStatusChange(wallet => {
-            if (wallet) {
-                status.innerText = `✅ Bağlanan cüzdan:\n${wallet.account.address}`;
-                tg.MainButton.setText('Kapat').show();
-                tg.sendData(JSON.stringify({ wallet: wallet.account.address }));
-            }
-        });
-    } else {
-        status.innerText = "Cüzdan bulunamadı!";
-    }
+    status.innerText = 'QR kodunu TON Wallet uygulaman ile okut!';
 };
+
+connector.onStatusChange(wallet => {
+    if (wallet) {
+        status.innerText = `✅ Cüzdan bağlandı:\n${wallet.account.address}`;
+        qrContainer.innerHTML = '';
+        tg.MainButton.setText('Kapat').show();
+        tg.sendData(JSON.stringify({ wallet: wallet.account.address }));
+    }
+});
 
 tg.MainButton.onClick(() => tg.close());
